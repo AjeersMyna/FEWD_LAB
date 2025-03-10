1805e58f5bf2fefbcb0663995ccdcaaf7dbb22c2
unknown <smynampati04@gmail.com>
2025-03-10 10:50:08 +0530

    Ecommerce-Bootstrap

---
+++ CLOUDCOMLAB/app.jsx
@@ -0,0 +1,13 @@
+import react from react;
+        function App(){
+            const x=5;
+            let text="GoodBye";
+            if(x<10){
+                text="Hello"
+            }
+            r=App();
+            return(
+                <div>
+                    <h1>{text}</h1>
+                </div>
+            )
